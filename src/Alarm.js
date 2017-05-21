7733a5f41005af626c2c98adeb33e4dc709f48ad
anishd19 <anishd19@gmail.com>
2017-05-21 15:06:50 +0530

    adds add button

--- src/Alarm.js
+++ src/Alarm.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Paper} from 'material-ui';
+import {Paper, FloatingActionButton} from 'material-ui';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 import OptionsBar from './OptionsBar'
 
 class Alarm extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <div className='stage-holder'>
         <Paper className='stage' zDepth={5} >
           <OptionsBar />
+        <FloatingActionButton className='add-btn' secondary={true}>
+          <ContentAdd />
+        </FloatingActionButton>
         </Paper>
       </div>
     )
