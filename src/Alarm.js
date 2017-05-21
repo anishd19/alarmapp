4870fda8486630d83ce133c342e79b209d1848aa
anishd19 <anishd19@gmail.com>
2017-05-21 18:02:41 +0530

    adds Timepicker on click of add button

--- src/Alarm.js
+++ src/Alarm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Paper, FloatingActionButton} from 'material-ui';
+import {Paper, FloatingActionButton, TimePicker} from 'material-ui';
 import ContentAdd from 'material-ui/svg-icons/content/add';
 import OptionsBar from './OptionsBar'
 import AlarmCard from './AlarmCard'
@@ -8,16 +8,33 @@ import AlarmCard from './AlarmCard'
 class Alarm extends Component {
   constructor(props) {
     super(props);
+    this.state = {alarmArray: []}
   }
+
+  handleChangeTimePicker12 = (event, date) => {
+    // this.setState({value12: date});
+    console.log('date is: ', date);
+  };
+
+  handleAddClick = () => {
+    this.refs.tp.openDialog()
+  };
+
   render() {
     return (
       <div className='stage-holder'>
         <Paper className='stage' zDepth={5} >
           <OptionsBar />
           <AlarmCard />
-          <FloatingActionButton className='add-btn' secondary={true}>
+          <FloatingActionButton className='add-btn' onClick={this.handleAddClick} secondary={true}>
             <ContentAdd />
           </FloatingActionButton>
+          <TimePicker
+            className='timepicker'
+           format="ampm"
+           onChange={this.handleChangeTimePicker12}
+           ref='tp'
+          />
         </Paper>
       </div>
     )
