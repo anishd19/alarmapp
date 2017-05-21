b0babb788e68e454da22bee26023b46fae9fe9ad
anishd19 <anishd19@gmail.com>
2017-05-21 10:30:01 +0530

    adds stage to the Alarm component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import {Paper} from 'material-ui';
+
+injectTapEventPlugin();
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h2>Start of the app development</h2>
+      <MuiThemeProvider>
+        <div className="App">
+          <Alarm />
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+class Alarm extends Component {
+  render() {
+    return (
+      <div className='stage-holder'>
+        <Paper className='stage' zDepth={5} >
+          <h2>Hello world</h2>
+        </Paper>
       </div>
     );
   }
